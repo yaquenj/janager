fa3cc4ed614c131101a1c9491b29555b302dc951
Kuba <yaquenjs@gmail.com>
2026-03-07 22:54:50 +0100

    Add tailwind color palette

---
+++ src/constants/TailwindColors.java
@@ -0,0 +1,35 @@
+package constants;
+
+import java.awt.*;
+
+public final class TailwindColors {
+
+    //? Prevent instantiation
+    private TailwindColors() {}
+
+    //? Tailwind color pallets
+    public static final Color SLATE_950 = new Color(2, 6, 24);
+    public static final Color SLATE_900 = new Color(15, 23, 43);
+    public static final Color SLATE_800 = new Color(29, 41, 61);
+    public static final Color SLATE_700 = new Color(49, 65, 88);
+    public static final Color SLATE_600 = new Color(69, 85, 108);
+    public static final Color SLATE_500 = new Color(98, 116, 142);
+    public static final Color SLATE_400 = new Color(98, 116, 142);
+    public static final Color SLATE_300 = new Color(202, 213, 226);
+    public static final Color SLATE_200 = new Color(226, 232, 240);
+    public static final Color SLATE_100 = new Color(241, 245, 249);
+    public static final Color SLATE_50  = new Color(248, 250, 252);
+
+    public static final Color INDIGO_900 = new Color(30, 26, 77);
+    public static final Color INDIGO_800 = new Color(49, 44, 133);
+    public static final Color INDIGO_950 = new Color(55, 42, 172);
+    public static final Color INDIGO_700 = new Color(67, 45, 215);
+    public static final Color INDIGO_600 = new Color(79, 57, 246);
+    public static final Color INDIGO_500 = new Color(97, 95, 255);
+    public static final Color INDIGO_400 = new Color(124, 134, 255);
+    public static final Color INDIGO_300 = new Color(163, 179, 255);
+    public static final Color INDIGO_200 = new Color(198, 210, 255);
+    public static final Color INDIGO_100 = new Color(224, 231, 255);
+    public static final Color INDIGO_50  = new Color(238, 242, 255);
+
+}
