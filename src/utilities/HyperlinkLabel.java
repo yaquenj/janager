0b0e9ce4a4b9926d8fd0164e6270a54cc2e94cb9
Kuba <yaquenjs@gmail.com>
2026-03-13 20:37:22 +0100

    create a universal class for Hyperlinks responsible for handling actions when clicked.

--- src/utilities/HyperlinkLabel.java
+++ src/utilities/HyperlinkLabel.java
@@ -5,36 +5,32 @@ import java.awt.Cursor;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import constants.TailwindColors;
+
 public class HyperlinkLabel extends JLabel {
 
     public HyperlinkLabel(String text, Runnable function) {
         setText("<html><a>" + text + "</a></html>");
+        setForeground(TailwindColors.INDIGO_600);
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 function.run();
+                setForeground(TailwindColors.INDIGO_400);
+                Timer timer = new Timer(50, event -> setForeground(TailwindColors.INDIGO_600));
+                timer.setRepeats(false);
+                timer.start();
             }
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
+            @Override public void mousePressed(MouseEvent e) {}
+            @Override public void mouseReleased(MouseEvent e) {}
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+                setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
             }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
+            @Override public void mouseExited(MouseEvent e) {}
         });
     }
-
 }
