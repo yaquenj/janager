f610783e87b4787d467ea2f4a22017dd883725b3
yaquenj <yaquenjs@gmail.com>
2026-03-28 22:54:08 +0100

    Hardcode user creation and retrieval example in `Main` using `DbUserUtils`

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import frames.AuthFrame;
+import models.User;
 import utilities.DatabaseInitializer;
+import utilities.DbUserUtils;
 
 public class Main {
     public static void main(String[] args) {
 
         //? First database initialization - does not overwrite it but creates a new one with necessary tables, if it doesn't exist
         DatabaseInitializer.init();
+        DbUserUtils.createUser(new User("yaquenj", "hasełko masełko", "sól"));
+        var usr = DbUserUtils.getUser("yaqeuenj");
         new AuthFrame();
 
     }
