1130e10803149d68d620586e9f199a31bc4797be
yaquenj <yaquenjs@gmail.com>
2026-03-29 18:12:00 +0200

    Remove hardcoded user creation and retrieval from `Main`; adjust `main` method to throw necessary exceptions.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 import frames.AuthFrame;
-import models.User;
 import utilities.DatabaseInitializer;
-import utilities.DbUserUtils;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException {
 
-        //? First database initialization - does not overwrite it but creates a new one with necessary tables, if it doesn't exist
+        // First database initialization - does not overwrite it but creates a new one with necessary tables, if it doesn't exist
         DatabaseInitializer.init();
-        DbUserUtils.createUser(new User("yaquenj", "hasełko masełko", "sól"));
-        var usr = DbUserUtils.getUser("yaqeuenj");
         new AuthFrame();
 
     }
